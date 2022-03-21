5e2b9a0a7a40237a49afd1d8ade8455a4ec7e9dd
Sung Yoo <yoowook1207@gmail.com>
2022-03-21 17:45:32 -0400

    comment

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRoutes);
+
 
 module.exports = router;
